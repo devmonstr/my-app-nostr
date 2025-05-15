d6d201c0973f949b106d9e43acd5e4e32a0b3366
devmonstr <159246932+devmonstr@users.noreply.github.com>
2025-05-15 11:02:00 +0700

    Added tailwind.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  allowedDevOrigins: ['http://192.168.1.96:3001', 'http://localhost:3001'],
+  output: 'standalone'
 };
 
 export default nextConfig;
